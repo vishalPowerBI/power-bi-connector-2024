154bf7a85659b7566f43a214e258089400912212
vishalPowerBI <145339268+vishalPowerBI@users.noreply.github.com>
2024-04-15 12:30:39 +0530

    Update __manifest__.py

--- power-bi-connector-2024/__manifest__.py
+++ power-bi-connector-2024/__manifest__.py
@@ -6,7 +6,7 @@
        	  
 Introducing the Direct Odoo Power BI Connector, the exclusive module that seamlessly integrates your Odoo platform with Power BI, offering enhanced features. Easily create Power BI Dashboards and Reports with automatic scheduled data refresh. This connector ensures a swift link between your Odoo data and the powerful Business Intelligence tool, Power BI Desktop.
 
-Experience advanced functionalities, such as publishing dashboards to Microsoft Teams or making them publicly accessible. Benefit from automatic scheduled refresh/reload, gateway support, and prebuilt tables relationship. This high-speed connector facilitates the transfer of data from Odoo to Power BI, providing an ER diagram-like feature to visualize relationships and columns in your tables.
+Experience advanced functionalities, powerbi such as publishing dashboards to Microsoft Teams or making them publicly accessible. Benefit from automatic scheduled refresh/reload, gateway support, and prebuilt tables relationship. This high-speed connector facilitates the transfer of data from Odoo to Power BI, providing an ER diagram-like feature to visualize relationships and columns in your tables.
 
 Explore a range of dashboards for Odoo ERP, including CRM, Inventory, Sales, Account, Invoice, and Revamp Dashboards. This connector is designed to revitalize your Odoo Dashboard, offering a comprehensive solution with features like Chart Graphs, Table Views, and all-in-one dynamic dashboards. It is a top-notch Odoo app in the market, ensuring a seamless connection between Odoo and Power BI for robust business intelligence.
        """,
